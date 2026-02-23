8927ee8b549763097c84bb390d24bf5e68771f97
New9c <aimccccccccc@gmail.com>
2026-02-23 14:59:16 +0800

    add middleware again

--- middleware.js
+++ middleware.js
@@ -1,19 +1,21 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
+export default function middleware(request) {
+    const url = new URL(request.url);
     const hostname = request.headers.get('host');
 
-    // Define your "Shortcuts" here
     const redirects = {
         'leetcode.danew9c.com': 'https://leetcode.com/u/9c-ccccccccc',
         'github.danew9c.com': 'https://github.com/New9c',
     };
 
-    // Check if the current subdomain exists in our list
+    // Check if the current hostname is in our list
     if (redirects[hostname]) {
-        return NextResponse.redirect(redirects[hostname], 301);
+        return Response.redirect(redirects[hostname], 301);
     }
 
-    // If no match, Vercel continues to serve your normal site files
-    return NextResponse.next();
+    // If no match, return nothing (Vercel will continue to your static files)
 }
+
+// This config tells Vercel to run this for EVERY request
+export const config = {
+    matcher: '/:path*',
+};
